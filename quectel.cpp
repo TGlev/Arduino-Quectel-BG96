80218bf0a6cbb7cd64b7640a23b2cb9e6fbce6f1
Unknown <118442@mymerewade.nl>
2019-11-09 17:08:21 +0100

    Added function headers

--- quectel.cpp
+++ quectel.cpp
@@ -9,13 +9,22 @@
 
 SoftwareSerial BG96Serial(RXPIN, TXPIN);
 
+/*
+    Starts the communication with the module by confirming that we have a UART connection
+    This is done by sending "AT" and waiting for the module to respond with "OK".
+*/
 bool Quectel::Begin()
 {
     BG96Serial.begin(BAUDRATE);
+    //TODO: send AT and wait for OK
+
     Serial.println("BG96 Started!");
     return true;
 }
 
+/*
+    Reads the given string and parses it into a byte that is more easily readable.
+*/
 byte Quectel::ParseResponse(String response)
 {
     if(response.indexOf("OK") > 0)
@@ -28,14 +37,23 @@ byte Quectel::ParseResponse(String response)
     return 0;
 }
 
+/*
+    Reads the string response from the BG96Serial buffer and returns it.
+    CHECKS FOREVER! ONLY USE WHEN YOU ARE SURE THE MODULE HAS RESPONDED!
+
+    TODO: Make this check for about a second instead of forever
+*/
 String Quectel::GetResponse()
 {
-    //TODO: Make this check for about a second instead of forever
     while(!BG96Serial.available());
-
     return BG96Serial.readString();
 }
 
+/*
+    Reads the incoming string command and prefixes it with "AT+".
+    After that, it sends the command trough the BG96Serial.
+    Returns the parsed response when done. 
+*/
 byte Quectel::SendCommand(String command)
 {
     String message;
@@ -50,6 +68,12 @@ byte Quectel::SendCommand(String command)
     return ParseResponse(response);
 }
 
+/*
+    Sets the APN to the given URL.
+    Does this by sending the QICSGP AT command.
+
+    TODO: implement support for apns that need a username/password
+*/
 bool Quectel::SetApn(String apn)
 {
     //Example for 1nce:
